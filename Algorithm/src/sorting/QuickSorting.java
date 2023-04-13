bea0d9e5983367a3a112430b9d866c2d9c218c75
codesver <codesver@gmail.com>
2023-04-13 14:47:40 +0900

    feat(QuickSorting): 퀵 정렬 구현

---
+++ Algorithm/src/sorting/QuickSorting.java
@@ -0,0 +1,33 @@
+package sorting;
+
+public class QuickSorting extends Sorting {
+
+    @Override
+    void sort(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+    }
+
+    @Override
+    void optimizedSort(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+    }
+
+    void quickSort(int[] nums, int left, int right) {
+        if (left >= right) return;
+        int pivot = partition(nums, left, right);
+        quickSort(nums, left, pivot - 1);
+        quickSort(nums, pivot + 1, right);
+    }
+
+    int partition(int[] nums, int left, int right) {
+        int pivot = nums[left];
+        int i = left, j = right;
+        while (i < j) {
+            while (pivot < nums[j]) j--;
+            while (i < j && pivot >= nums[i]) i++;
+            swap(nums, i, j);
+        }
+        swap(nums, left, i);
+        return i;
+    }
+}
