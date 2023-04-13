dff19383c62e22aca27a3358c708d619852b71c5
codesver <codesver@gmail.com>
2023-04-13 18:13:34 +0900

    feat(FloydWarshall): 플로이드-워샬 구현

---
+++ Algorithm/src/graph/FloydWarshall.java
@@ -0,0 +1,25 @@
+package graph;
+
+public class FloydWarshall {
+
+    private final int MAX_VALUE = Integer.MAX_VALUE; // Sum of all cost + 1
+
+    int[][] solve(int[][] graph) {
+        int[][] distances = new int[graph.length][graph.length];
+        for (int from = 0; from < graph.length; from++)
+            for (int to = 0; to < graph.length; to++)
+                distances[from][to] = graph[from][to] == 0 ?
+                        MAX_VALUE :
+                        graph[from][to];
+
+        for (int mid = 0; mid < graph.length; mid++)
+            for (int from = 0; from < graph.length; from++)
+                for (int to = 0; to < graph.length; to++)
+                    distances[from][to] = Math.min(
+                            distances[from][to],
+                            distances[from][mid] = distances[mid][to]
+                    );
+
+        return distances;
+    }
+}
