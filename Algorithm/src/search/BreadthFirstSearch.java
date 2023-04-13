1359411ee7c4f0c1697d6dc6c591f97bf8d06c4c
codesver <codesver@gmail.com>
2023-04-13 16:33:06 +0900

    feat(BreadthFirstSearch): 너비 우선 탐색 구현

---
+++ Algorithm/src/search/BreadthFirstSearch.java
@@ -0,0 +1,24 @@
+package search;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BreadthFirstSearch extends Search {
+
+    @Override
+    void search(boolean[][] map, int start) {
+        boolean[] visited = new boolean[map.length];
+        visited[start] = true;
+        Queue<Integer> queue = new LinkedList<>(Collections.singleton(start));
+        while (!queue.isEmpty()) {
+            int num = queue.poll();
+            for (int i = 0; i < map[num].length; i++) {
+                if (map[num][i] && !visited[i]) {
+                    queue.offer(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+}
